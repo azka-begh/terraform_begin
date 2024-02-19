72a72d64c956abf5b94555e73000d7b7d12f84a1
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-19 19:07:35 +0530

    Update output_values.tf

--- output_values.tf
+++ output_values.tf
@@ -4,6 +4,15 @@ provider "aws" {
 resource "aws_eip" "myip" {
   domain = "vpc"
 }
+//Prints the IP
 output "public-ip" {
   value = aws_eip.myip.public_ip
+}
+//Prints a URL
+output "url" {
+  value = "https://${aws_eip.myip.public_ip}:8080"
+}
+//Prints elastic ip details
+output "details" {
+  value = "aws_eip.myip"
 }
