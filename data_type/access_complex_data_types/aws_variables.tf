c52b77a8222c97fb3553f18162339407b8f2eefc
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-20 19:59:30 +0530

    Update aws_variables.tf

--- data_type/access_complex_data_types/aws_variables.tf
+++ data_type/access_complex_data_types/aws_variables.tf
@@ -2,26 +2,26 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "myinstance1" {
-  ami = "ami-05fb0b8c1424f266b"
-  instance_type = var.mylist[1]
-}
-
-resource "aws_instance" "myinstance2" {
-  ami = "ami-05fb0b8c1424f266b"
-  instance_type = var.mymap["us-east-2"]
-}
-
 variable "mylist" {
-  type = list
+  type    = list(any)
   default = ["t2.micro", "t3.micro", "t2.medium"]
 }
 
 variable "mymap" {
-  type = map
+  type = map(any)
   default = {
-    us-east-2 = "t2.micro"
-    us-east-1 = "t2.medium"
+    us-east-2  = "t2.micro"
+    us-east-1  = "t2.medium"
     ap-south-1 = "t3.micro"
   }
+}
+
+resource "aws_instance" "myinstance1" {
+  ami           = "ami-05fb0b8c1424f266b"
+  instance_type = var.mylist[1]
+}
+
+resource "aws_instance" "myinstance2" {
+  ami           = "ami-05fb0b8c1424f266b"
+  instance_type = var.mymap["us-east-2"]
 }
