cbb221e3fa2d947cead8ad158e5a9ab21b3f5d28
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-20 20:07:45 +0530

    Create 3_count_index.tf

---
+++ count_parameter/3_count_index.tf
@@ -0,0 +1,25 @@
+# variables.tf
+variable "ami" {
+  type    = string
+  default = "ami-0078ef784b6fa1ba4"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "servers" {
+  type    = list(string)
+  default = ["server_one", "server_two", "server_three"]
+}
+
+# main.tf
+resource "aws_instance" "myinstance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  count         = length(var.servers)
+  tags = {
+    Name = var.sandboxes[count.index]
+  }
+}
