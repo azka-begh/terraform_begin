b7a0fa52ecc43d771752cb4afd1d0ecd29219c18
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-20 19:18:46 +0530

    Update count_aws-instances.tf

--- count_parameter/count_aws-instances.tf
+++ count_parameter/count_aws-instances.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "myinstance" {
-    ami = "ami-05fb0b8c1424f266b"
-    instance_type = "t2.micro"
-    count = 3
+  ami           = "ami-05fb0b8c1424f266b"
+  instance_type = "t2.micro"
+  count         = 3
 }
