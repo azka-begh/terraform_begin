a7a2ed8e32e40da5439c683ef38576be9b6a61f5
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-21 20:55:51 +0530

    Update aws_vpc_existing.tf

--- data_sources/aws_vpc_existing.tf
+++ data_sources/aws_vpc_existing.tf
@@ -7,7 +7,7 @@ data "aws_vpc" "myvpc" {
   filter {
     # It will get data of vpn whose name (which is actually a tag with key "Name") is "default". This is how filter works.
     name   = "tag:Name"
-    values = ["default"]
+    values = ["tf_vpc"]
   }
 }
 
