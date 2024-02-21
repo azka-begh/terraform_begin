cb0fd8adef6756ba63fce81a4552c3ebb6bd4fe6
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-21 19:08:37 +0530

    Update data_source_block.tf

--- data_sources/data_source_block.tf
+++ data_sources/data_source_block.tf
@@ -1,2 +1,21 @@
 provider "aws" {
-  
+  region = "us-east-2"
+}
+
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvn*"]
+  }
+}
+
+resource "aws_instance" "myinstance" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t2.micro"
+  tags = {
+    Name = my_vm
+  }
+}
