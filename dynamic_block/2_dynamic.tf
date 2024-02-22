2fed62163170d3d18fbcc2d16dceaa55b45fd652
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-22 15:36:25 +0530

    Create 2_dynamic.tf

---
+++ dynamic_block/2_dynamic.tf
@@ -0,0 +1,41 @@
+variable "settings" {
+  type = list(object({
+    description = string
+    port        = number
+  }))
+  default = [
+    {
+      description = "Allows SSH access"
+      port        = 22
+    },
+    {
+      description = "Allows HTTP traffic"
+      port        = 80
+    },
+    {
+      description = "Allows HTTPS traffic"
+      port        = 443
+    }
+  ]
+}
+
+resource "aws_security_group" "sandbox_sg" {
+  name   = "sandbox_sg"
+  vpc_id = aws_vpc.sandbox_vpc.id
+
+  dynamic "ingress" {
+    for_each = var.settings
+
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = [aws_vpc.sandbox_vpc.cidr_block]
+    }
+  }
+
+  tags = {
+    Name = "sandbox_sg"
+  }
+}
