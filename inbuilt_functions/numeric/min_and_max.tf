d87d5b706644ba4716345237cb6a11fa01b5e562
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-23 17:43:11 +0530

    Update min_and_max.tf

--- inbuilt_functions/numeric/min_and_max.tf
+++ inbuilt_functions/numeric/min_and_max.tf
@@ -1,13 +1,23 @@
-terraform console
+# Numeric Functions
 
+```terraform console```
+
+1. **max:**
+```
 > max(12, 54, 3)
 54
-
+```
+```
 > max([12, 54, 3]...)
 54
+```
 
+2. **min:**
+```
 > min(12, 54, 3)
 3
-
+```
+```
 > min([12, 54, 3]...)
 3
+```
