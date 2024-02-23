834bf94cfc0461b1d6e13bf391e0031f6cdebcd4
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-23 11:25:26 +0530

    Create lifecycle1.tf

---
+++ meta_arguments/lifecycle1.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "ec2-b" {
+  ami           = "ami-05fb0b8c1424f266b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my_vm"
+  }
+
+  #meta-argument -> lifeycle
+  lifecycle {
+     ignore_changes = [tags]
+ }
+}
+
