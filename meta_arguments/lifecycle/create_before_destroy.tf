600b62b141d9d1869f8c254697109b80a48386ba
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-23 11:57:28 +0530

    Create create_before_destroy.tf

---
+++ meta_arguments/lifecycle/create_before_destroy.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "ec2-b" {
+  ami           = "ami-05fb0b8c1424f266b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "my_vm1"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+ }
+}
