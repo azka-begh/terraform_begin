18c9d5054bec57a48c1ace480ec4f4b8f497e814
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-23 15:16:33 +0530

    Delete count_meta_argument directory

--- count_meta_argument/count_aws-instances.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-2"
-}
-
-resource "aws_instance" "myinstance" {
-  ami           = "ami-05fb0b8c1424f266b"
-  instance_type = "t2.micro"
-  count         = 3
-}
