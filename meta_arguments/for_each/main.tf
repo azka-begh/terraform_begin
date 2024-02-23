d3d1d776b244eb179ced1e8bf931f6520047f435
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-23 14:59:48 +0530

    Update main.tf

--- meta_arguments/for_each/main.tf
+++ meta_arguments/for_each/main.tf
@@ -1,1 +1,24 @@
+variable "ami" {
+  type    = string
+  default = "ami-0078ef784b6fa1ba4"
+}
 
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "instances" {
+  type    = list(string)
+  default = ["server1", "server2", "server3"]
+}
+
+resource "aws_instance" "myec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  for_each      = toset(var.instances)
+  tags = {
+    Name = each.value 
+    # for a set, each.value and each.key is the same
+  }
+}
