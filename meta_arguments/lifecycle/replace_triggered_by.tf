4770dea0cbbfbea70a8005e5192449899a87da66
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-23 14:02:34 +0530

    Update replace_triggered_by.tf

--- meta_arguments/lifecycle/replace_triggered_by.tf
+++ meta_arguments/lifecycle/replace_triggered_by.tf
@@ -1,2 +1,10 @@
-<
-"""""""""}
+resource "aws_appautoscaling_target" "ecs_target" {
+  # ...
+  lifecycle {
+    replace_triggered_by = [
+      # Replace `aws_appautoscaling_target` each time this instance of
+      # the `aws_ecs_service` is replaced.
+      aws_ecs_service.svc.id
+    ]
+  }
+}
