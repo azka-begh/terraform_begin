fe990dabd369fa6ae1911a58dc15259944c6d4d4
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-24 12:49:32 +0530

    Update create_ec2_using_existing_key.tf

--- data_sources/create_ec2_using_existing_key.tf
+++ data_sources/create_ec2_using_existing_key.tf
@@ -3,8 +3,27 @@ data "aws_key_pair" "terraform" {
   include_public_key = true
 
   filter {
-    name = "tag:Name"
+    name   = "tag:Name"
     values = ["terraform"]
   }
 }
 
+data "aws_ami" "ami_ubuntu" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64*"]
+  }
+}
+
+resource "aws_instance" "myinstance" {
+  ami           = data.aws_ami.ami_ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = data.aws_key_pair.terraform.key_name
+
+  tags = {
+    Name = "ubuntu_vm"
+  }
+}
