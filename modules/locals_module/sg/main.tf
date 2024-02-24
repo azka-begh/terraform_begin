6a7410cfbc7c94b265d5dc6b2710a865f2e85717
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-24 17:51:23 +0530

    Update main.tf

--- modules/locals_module/sg/main.tf
+++ modules/locals_module/sg/main.tf
@@ -7,14 +7,14 @@ resource "aws_security_group" "ec2_sg" {
   }
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    from_port   = 443
+    from_port   = local.app_port
     protocol    = "tcp"
-    to_port     = 443
+    to_port     = local.app_port
   }
 
   egress {
     cidr_blocks = ["0.0.0.0/0"]
-    protocol    = "-1" 
+    protocol    = "-1"
     from_port   = 0
     to_port     = 0
   }
@@ -25,15 +25,15 @@ resource "aws_security_group" "elb_sg" {
 
   ingress {
     description = "Allow Inbound from Secret Application"
-    from_port   = 8443
-    to_port     = 8443
+    from_port   = local.elb_port
+    to_port     = local.elb_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     cidr_blocks = ["0.0.0.0/0"]
-    protocol    = "-1" 
+    protocol    = "-1"
     from_port   = 0
     to_port     = 0
   }
