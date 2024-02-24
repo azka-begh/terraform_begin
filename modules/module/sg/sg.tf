60d0151c0c1ec5e243162501a36f279e9ca1d082
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-24 16:53:18 +0530

    Create sg.tf

---
+++ modules/module/sg/sg.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "ec2_sg"
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    protocol    = "tcp"
+    to_port     = 443
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1" 
+    from_port   = 0
+    to_port     = 0
+  }
+}
+
+resource "aws_security_group" "elb_sg" {
+  name = "elb_sg"
+
+  ingress {
+    description = "Allow Inbound from Secret Application"
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1" 
+    from_port   = 0
+    to_port     = 0
+  }
+}
