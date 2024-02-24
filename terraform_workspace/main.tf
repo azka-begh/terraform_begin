23bca875ae4c2a126081075c72f3e90381995264
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-24 21:27:05 +0530

    Update and rename ec2_in_different_workspaces.tf to main.tf

--- terraform_workspace/main.tf
+++ terraform_workspace/main.tf
@@ -7,12 +7,4 @@ resource "aws_instance" "myec2" {
   instance_type = lookup(var.instance_type, terraform.workspace)
 }
 
-variable "instance_type" {
-  type = map(string)
-
-  default = {
-    default = "t2.nano"
-    dev     = "t2.micro"
-    prod    = "t2.large"
-  }
 }
