ac581d271ae0494e097e34453a50499aa9a3f275
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-24 12:08:17 +0530

    Update remote_exec.tf

--- provisioners/remote_exec.tf
+++ provisioners/remote_exec.tf
@@ -2,21 +2,25 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# Creates a private key
 resource "tls_private_key" "rsa_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+# Creates a key pair using tls_private_key
 resource "aws_key_pair" "ec2_pub_key" {
   key_name   = "terraform"
   public_key = tls_private_key.rsa_key.public_key_openssh
 }
 
+# Saves the private key to your local (pwd)
 resource "local_file" "ec2_priv_key" {
   content  = tls_private_key.rsa_key.private_key_pem
   filename = "terraform.pem"
 }
 
+# Creates instance and attaches the public key to it
 resource "aws_instance" "ec2" {
   ami           = "ami-05fb0b8c1424f266b"
   instance_type = "t2.micro"
@@ -25,18 +29,20 @@ resource "aws_instance" "ec2" {
     Name = "ubuntu-1"
   }
 
+# Using the private key created above, connection is amde to the instance
   connection {
-    type = "ssh"
-    user = "ubuntu"
+    type        = "ssh"
+    user        = "ubuntu"
     private_key = tls_private_key.rsa_key.private_key_pem
-    host = self.public_ip
+    host        = self.public_ip
   }
 
+# Commands are run remotely on the instance
   provisioner "remote-exec" {
-    inline = [ 
-        "sudo apt update -y",
-        "sudo apt install nginx -y",
-        "sudo systemctl start nginx"
-     ]
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install nginx -y",
+      "sudo systemctl start nginx"
+    ]
   }
 }
