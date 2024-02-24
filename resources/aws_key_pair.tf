b31608dc8b3cefa4f33b30c236add80962d54edb
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-24 11:30:16 +0530

    Update aws_key_pair.tf

--- resources/aws_key_pair.tf
+++ resources/aws_key_pair.tf
@@ -7,12 +7,21 @@ resource "tls_private_key" "rsa_key" {
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "ec2-key" {
+resource "aws_key_pair" "ec2_pub_key" {
   key_name   = "terraform"
   public_key = tls_private_key.rsa_key.public_key_openssh
 }
 
-resource "local_file" "terraform_local_key" {
+resource "local_file" "ec2_priv_key" {
   content  = tls_private_key.rsa_key.private_key_pem
-  filename = "terraform_key.pem"
+  filename = "terraform.pem"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-05fb0b8c1424f266b"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.ec2_pub_key.key_name
+
+  tags {
+    Name = "ubuntu-1"
 }
