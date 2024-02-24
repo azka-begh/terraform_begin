f6e22964af88e6e81c045f6dc826d793a5ca9ee1
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-24 18:25:27 +0530

    Update my_sg_with_ec2.tf

--- modules/C/my_sg_with_ec2.tf
+++ modules/C/my_sg_with_ec2.tf
@@ -3,7 +3,7 @@ module "sgmodule" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = ""
+  ami                    = "ami-0265ca2039df27f61"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [module.sgmodule.sg_id]
 }
