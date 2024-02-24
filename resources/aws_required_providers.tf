eea47faf780f8ad2b64c59da443a6fc46bce9fd9
azka13 <97597065+begh-azka@users.noreply.github.com>
2024-02-24 11:34:53 +0530

    Update aws_required_providers.tf

--- resources/aws_required_providers.tf
+++ resources/aws_required_providers.tf
@@ -1,21 +1,27 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">=3.27.0,<=4.0.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "instance-res" {
-  instance_type = "t2.micro"
-  ami           = "ami-05fb0b8c1424f266b"
+resource "tls_private_key" "rsa_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
+resource "aws_key_pair" "ec2_pub_key" {
+  key_name   = "terraform"
+  public_key = tls_private_key.rsa_key.public_key_openssh
+}
+
+resource "local_file" "ec2_priv_key" {
+  content  = tls_private_key.rsa_key.private_key_pem
+  filename = "terraform.pem"
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-05fb0b8c1424f266b"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.ec2_pub_key.key_name
   tags = {
-    Name = "vm1"
+    Name = "ubuntu-1"
   }
 }
